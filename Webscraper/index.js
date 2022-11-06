74917b40ab7328319cedbf91089e6ba56ee9f54e
maet0 <hymer122333@gmail.com>
2022-11-06 17:50:36 +0100

    Added further gps API results

--- Webscraper/index.js
+++ Webscraper/index.js
@@ -9,6 +9,9 @@ const sanitize = require('sanitize-filename');//Using this npm module to sanitiz
 const fs = require('fs');
 const { Scraper, Root, OpenLinks } = require('nodejs-web-scraper');
 
+var driUrl;
+
+
 function runTagScraper(){
     var invoked = false
     var process = childProcess
@@ -20,19 +23,20 @@ function runTagScraper(){
         callback(err)
     })
 }
+
+// Scrape for Google/Facebook/LinkedIn Tags
 function scrapeTags(){
 
     (async () => {
+        console.log(driUrl)
         const config = {
-            baseSiteUrl: `https://www.profesia.sk`,
-            startUrl: `https://www.profesia.sk/praca/`,
-            removeStyleAndScriptTags: false//Telling the scraper NOT to remove style and script tags, cause i want it in my html files, for this example.        
+            baseSiteUrl: `${driUrl}`,
+            startUrl: `${driUrl}`,
+            removeStyleAndScriptTags: false //Telling the scraper NOT to remove style and script tags
         }
     
-        let directoryExists;
-    
         const getPageHtml = (html, pageAddress) => {//Saving the HTML file, using the page address as a name.
-    
+
             if(!directoryExists){
                 fs.mkdirSync('./html');
                 directoryExists = true;
@@ -40,15 +44,9 @@ function scrapeTags(){
             const name = sanitize(pageAddress)
             fs.writeFile(`./html/${name}.html`, html, () => { })
         }
-    
+
         const scraper = new Scraper(config);
-    
         const root = new Root({});
-    
-        const jobAds = new OpenLinks('.list-row h2 a', { getPageHtml });//Opens every job ad, and calls a hook after every page is done.
-    
-        root.addOperation(jobAds);
-    
         await scraper.scrape(root);
     })() 
 
@@ -62,15 +60,13 @@ app.get('/', function (req, res) {
 
 app.get('/getDRI', function (req, res) { 
     var compdata
-    const pgsUrl = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${req.query.text}&key=${pgsAuthKey}`
-
-    scrapeTags()
+    const pgsUrl = `https://pagespeedonline.googleapis.com/pagespeedonline/v5/runPagespeed?category=ACCESSIBILITY&category=PERFORMANCE&category=SEO&locale=pt&strategy=DESKTOP&url=${req.query.text}&prettyPrint=true&key=${pgsAuthKey}`
 
     axios(pgsUrl).then(response => {
-        console.log(response.data)
         compdata = response.data
-        res.send(compdata)
+        res.send(response.data)
     })
+    console.log(compdata)
 }) 
 
 app.get('/results', (req, res) => {
@@ -89,6 +85,8 @@ app.get('/results', (req, res) => {
             const website = $(this).find('[itemprop=url]').text()
 
             companies.push({ title, street, zip, city,website, url })
+
+            driUrl = website
         }) 
         res.json(companies)
     }).catch(err => console.log(err))
