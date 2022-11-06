536b709969e5a93146d3bfdcf27b48853ed5895e
maet0 <hymer122333@gmail.com>
2022-11-06 16:55:39 +0100

    Implemented HTML Scraping

--- Webscraper/index.js
+++ Webscraper/index.js
@@ -5,10 +5,54 @@ const express = require('express')
 const app = express() 
 const cors = require('cors')
 const pgsAuthKey = 'AIzaSyDCqSuQrMeDBvf-srs7SL4cyPL-4YvsCUA'
-var childProcess = require('child_process').fork('index.js')
-const { response } = require('express')
+const sanitize = require('sanitize-filename');//Using this npm module to sanitize file names.
+const fs = require('fs');
+const { Scraper, Root, OpenLinks } = require('nodejs-web-scraper');
 
+function runTagScraper(){
+    var invoked = false
+    var process = childProcess
 
+    // listen for errors
+    process.on('error', function (err) {
+        if (invoked) return;
+        invoked = true
+        callback(err)
+    })
+}
+function scrapeTags(){
+
+    (async () => {
+        const config = {
+            baseSiteUrl: `https://www.profesia.sk`,
+            startUrl: `https://www.profesia.sk/praca/`,
+            removeStyleAndScriptTags: false//Telling the scraper NOT to remove style and script tags, cause i want it in my html files, for this example.        
+        }
+    
+        let directoryExists;
+    
+        const getPageHtml = (html, pageAddress) => {//Saving the HTML file, using the page address as a name.
+    
+            if(!directoryExists){
+                fs.mkdirSync('./html');
+                directoryExists = true;
+            }
+            const name = sanitize(pageAddress)
+            fs.writeFile(`./html/${name}.html`, html, () => { })
+        }
+    
+        const scraper = new Scraper(config);
+    
+        const root = new Root({});
+    
+        const jobAds = new OpenLinks('.list-row h2 a', { getPageHtml });//Opens every job ad, and calls a hook after every page is done.
+    
+        root.addOperation(jobAds);
+    
+        await scraper.scrape(root);
+    })() 
+
+}
 
 app.use(cors()) 
 
@@ -19,6 +63,8 @@ app.get('/', function (req, res) {
 app.get('/getDRI', function (req, res) { 
     var compdata
     const pgsUrl = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${req.query.text}&key=${pgsAuthKey}`
+
+    scrapeTags()
 
     axios(pgsUrl).then(response => {
         console.log(response.data)
