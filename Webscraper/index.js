755f4a5f568ecf073df6bc599ed1ce86171dc74c
Michal Stencel <stencel@spectory.at>
2022-11-19 21:52:45 +0100

    added functionality

--- Webscraper/index.js
+++ Webscraper/index.js
@@ -1,8 +1,8 @@
-const PORT = 8000 
-const axios = require('axios') 
-const cheerio = require('cheerio') 
-const express = require('express') 
-const app = express() 
+const PORT = 8000
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+const app = express()
 const cors = require('cors')
 const pgsAuthKey = 'AIzaSyDCqSuQrMeDBvf-srs7SL4cyPL-4YvsCUA'
 const sanitize = require('sanitize-filename');//Using this npm module to sanitize file names.
@@ -12,7 +12,7 @@ const { Scraper, Root, OpenLinks } = require('nodejs-web-scraper');
 var driUrl;
 
 
-function runTagScraper(){
+function runTagScraper() {
     var invoked = false
     var process = childProcess
 
@@ -25,7 +25,7 @@ function runTagScraper(){
 }
 
 // Scrape for Google/Facebook/LinkedIn Tags
-function scrapeTags(){
+function scrapeTags() {
 
     (async () => {
         console.log(driUrl)
@@ -34,10 +34,10 @@ function scrapeTags(){
             startUrl: `${driUrl}`,
             removeStyleAndScriptTags: false //Telling the scraper NOT to remove style and script tags
         }
-    
+
         const getPageHtml = (html, pageAddress) => {//Saving the HTML file, using the page address as a name.
 
-            if(!directoryExists){
+            if (!directoryExists) {
                 fs.mkdirSync('./html');
                 directoryExists = true;
             }
@@ -48,52 +48,58 @@ function scrapeTags(){
         const scraper = new Scraper(config);
         const root = new Root({});
         await scraper.scrape(root);
-    })() 
+    })()
 
 }
 
-app.use(cors()) 
+app.use(cors())
 
-app.get('/', function (req, res) { 
-    res.json('This is our DRI Webscraper') 
-}) 
+app.get('/', function (req, res) {
+    res.json('This is our DRI Webscraper')
+})
 
-app.get('/getDRI', function (req, res) { 
-    var compdata
-    const pgsUrl = `https://pagespeedonline.googleapis.com/pagespeedonline/v5/runPagespeed?category=ACCESSIBILITY&category=PERFORMANCE&category=SEO&locale=pt&strategy=DESKTOP&url=${req.query.text}&prettyPrint=true&key=${pgsAuthKey}`
-    var karriereseite = false;
-    // scrapeTags(karriereseite)
+app.get('/getDRI', function (req, res) {
+    let user = JSON.parse(req.query.user);
+    let company = JSON.parse(req.query.company);
+    if (company.website) {
+        var compdata
+        const pgsUrl = `https://pagespeedonline.googleapis.com/pagespeedonline/v5/runPagespeed?category=ACCESSIBILITY&category=PERFORMANCE&category=SEO&locale=pt&strategy=DESKTOP&url=${company.website}&prettyPrint=true&key=${pgsAuthKey}`
+        var karriereseite = false;
+        // scrapeTags(karriereseite)
 
-    axios(pgsUrl).then(response => {
-        compdata = response.data
-        res.send(response.data.lighthouseResult.categories)
-        console.log(response.data.lighthouseResult.categories.performance.score + " : Performance")
-        console.log(response.data.lighthouseResult.categories.accessibility.score + " : Barrierefreiheit")
-        console.log(response.data.lighthouseResult.categories.seo.score + " : SEO Rating")
-    })
-}) 
+        axios(pgsUrl).then(response => {
+            compdata = response.data
+            res.send(response.data.lighthouseResult.categories)
+            console.log(response.data.lighthouseResult.categories.performance.score + " : Performance")
+            console.log(response.data.lighthouseResult.categories.accessibility.score + " : Barrierefreiheit")
+            console.log(response.data.lighthouseResult.categories.seo.score + " : SEO Rating")
+        })
+    } else {
+        // Auf Google nach Website suchen
+    }
+})
 
 app.get('/results', (req, res) => {
-    let querytext = req.query?.text; 
-    const url = `https://firmen.wko.at/${querytext}`  
+    let querytext = req.query?.text;
+    const url = `https://firmen.wko.at/${querytext}`
     axios(url).then(response => {
-        const html = response.data 
-        const $ = cheerio.load(html) 
-        let companies = [] 
-        $('article', html).each(function () { 
-            const title = $(this).find('h3').text() 
-            const street = $(this).find('.street').text() 
-            const zip = $(this).find('.zip').text()?.split(' ')[0] 
-            const city = $(this).find('.locality').text() 
+        const html = response.data
+        const $ = cheerio.load(html)
+        let companies = []
+        $('article', html).each(function () {
+            const title = $(this).find('h3').text()
+            const street = $(this).find('.street').text()
+            const zip = $(this).find('.zip').text()?.split(' ')[0]
+            const city = $(this).find('.locality').text()
             const url = $(this).find('a').attr('href')
             const website = $(this).find('[itemprop=url]').text()
 
-            companies.push({ title, street, zip, city,website, url })
+            companies.push({ title, street, zip, city, website, url })
 
             driUrl = website
-        }) 
+        })
         res.json(companies)
     }).catch(err => console.log(err))
-}) 
+})
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
